4ee49c8b7f3cbc3d86dcb14928f73a1b097289be
Kevin Kemboi <kkemboi08@gmail.com>
2025-08-02 12:11:07 +0300

    Week1 Assignment

---
+++ calculator.py
@@ -0,0 +1,25 @@
+num1 = float(input("Enter the first number: "))
+
+num2 = float(input("Enter the second number: "))
+
+
+sum_result = num1 + num2
+
+
+difference_result = num1 - num2
+
+
+product_result = num1 * num2
+
+
+if num2 != 0:
+    quotient_result = num1 / num2
+else:
+    quotient_result = "Undefined (can't divide by zero!)"
+
+
+print("\n Here are your results! ")
+print(f" Sum: {sum_result}")
+print(f" Difference: {difference_result}")
+print(f" Product: {product_result}")
+print(f" Quotient: {quotient_result}")
